40121403d2c28535891d2cbca2ac32f42395f823
Hazelnut-Tree <89808846+Hazelnut-Tree@users.noreply.github.com>
2021-11-16 00:04:08 +0400

    BlendTree

--- Assets/Scripts/YBotController.cs
+++ Assets/Scripts/YBotController.cs
@@ -1,24 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class YBotController : MonoBehaviour
 {
     Animator animator;
+    NavMeshAgent agent;
+
+    int isWalkingHash;
+    int velocityZHash;
+    int velocityXHash;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
+        isWalkingHash = Animator.StringToHash("isWalking");
+        velocityXHash = Animator.StringToHash("velocityX");
+        velocityZHash = Animator.StringToHash("velocityZ");
+
+
 
     }
 
     private void Update()
     {
-        transform.position += transform.forward * Time.deltaTime;
-    }
+        Vector3 velocity = agent.velocity;
+        bool isMoving = agent.velocity.magnitude > 0.01f && agent.remainingDistance > agent.radius;
 
-    private void FixedUpdate()
-    {
-        animator.SetBool("IsWalking", true);
+        animator.SetBool("isWalking", isMoving);
+
+        velocity = transform.InverseTransformVector(velocity);
+
+        animator.SetFloat("velocityXHash", velocity.x);
+        animator.SetFloat("velocityZHash", velocity.z);
+
+
     }
 }
